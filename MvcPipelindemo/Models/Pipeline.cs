dafa9eb177a9180668fe465135f2272f6a27025a
chijekwu <chijekwu@users.noreply.github.com>
2020-05-14 20:42:58 -0400

    new

---
+++ MvcPipelindemo/Models/Pipeline.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcPipelindemo.Models
+{
+    public class Pipeline
+    {
+        public int Length { get; set; }
+        public int Width { get; set; }
+        public int Throughput{get;set;}
+        public int Depth { get; set; }
+    }
+}
